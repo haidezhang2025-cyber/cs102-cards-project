4ee36d6cfbc666a0cae07cfd2885865530bf5eea
Eden <64953425+Solaireis@users.noreply.github.com>
2026-03-30 18:49:12 +0800

    Modifed run controls

--- Splendor/src/Test/run.sh
+++ Splendor/src/Test/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 #make sure the directory is splendor
-java -cp classes Test.game
+#pwd
+cp config.properties ../../classes/config.properties
+#pwd
+#cd ../../
+java -cp ../../classes Test.Game
